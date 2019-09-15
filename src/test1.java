1cda53f797acc6b055df8e28fdf9094944b12c6b
francoisauxietre <Fafa1234!!>
2019-09-16 01:02:15 +0200

    debut ajout d'un test de servlet

--- src/test1.java
+++ src/test1.java
@@ -1,10 +1,10 @@
-import javax.servlet.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class test1 extends HttpServlet{
+public class test1 extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
